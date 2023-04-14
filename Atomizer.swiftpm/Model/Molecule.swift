3fd326c3876669f4a465305562c0ff89a9fe9e52
John Seong <wonmor@gmail.com>
2023-04-13 22:08:04 -0400

    Added molecules.json

---
+++ Atomizer.swiftpm/Model/Molecule.swift
@@ -0,0 +1,43 @@
+//
+//  File.swift
+//  
+//
+//  Created by John Seong on 2023-04-13.
+//
+
+import Foundation
+
+class Molecule: Identifiable, Decodable {
+    let id = UUID()
+    let name: String
+    let formula: String
+    let description: String
+    let shape: String
+    let polarity: String
+    let bondAngle: String
+    let orbitals: String
+    let hybridization: String
+    let molecularGeometry: String
+    let bonds: String
+    let imageURL: URL?
+    
+    enum CodingKeys: String, CodingKey {
+        case name, formula, description, shape, polarity, bondAngle, orbitals, hybridization, molecularGeometry, bonds
+        case imageURL = "imageURL"
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        formula = try container.decode(String.self, forKey: .formula)
+        description = try container.decode(String.self, forKey: .description)
+        shape = try container.decode(String.self, forKey: .shape)
+        polarity = try container.decode(String.self, forKey: .polarity)
+        bondAngle = try container.decode(String.self, forKey: .bondAngle)
+        orbitals = try container.decode(String.self, forKey: .orbitals)
+        hybridization = try container.decode(String.self, forKey: .hybridization)
+        molecularGeometry = try container.decode(String.self, forKey: .molecularGeometry)
+        bonds = try container.decode(String.self, forKey: .bonds)
+        imageURL = try container.decodeIfPresent(URL.self, forKey: .imageURL)
+    }
+}
--- Atomizer.swiftpm/View/MoleculeView.swift
+++ Atomizer.swiftpm/View/MoleculeView.swift
@@ -1,42 +1,33 @@
 import SwiftUI
 import Kingfisher
 
-struct Molecule: Identifiable {
-    let id = UUID()
-    let name: String
-    let formula: String
-    let imageURL: URL?
-}
-
-let molecules = [
-    Molecule(name: "Ethene", formula: "C2H4", imageURL: URL(string: "https://webbook.nist.gov/cgi/cbook.cgi?Struct=C74851&Type=Color")),
-    Molecule(name: "Water", formula: "H2O", imageURL: URL(string: "https://webbook.nist.gov/cgi/cbook.cgi?Struct=C7732185&Type=Color")),
-    Molecule(name: "Hydrogen", formula: "H2", imageURL: URL(string: "https://webbook.nist.gov/cgi/cbook.cgi?Struct=C1333740&Type=Color")),
-    Molecule(name: "Hydrochloric Acid", formula: "HCl", imageURL: URL(string: "https://webbook.nist.gov/cgi/cbook.cgi?Struct=C7647010&Type=Color")),
-    Molecule(name: "Chlorine", formula: "Cl2", imageURL: URL(string: "https://webbook.nist.gov/cgi/cbook.cgi?Struct=C7782505&Type=Color"))
-]
-
 struct MoleculeView: View {
     @Environment(\.horizontalSizeClass) var sizeClass
+    
+    @State var molecules: [Molecule] = []
     
     var body: some View {
         if sizeClass == .regular {
             let columns = [
-                    GridItem(.flexible(minimum: 180), spacing: 24), // Use flexible GridItem with minimum width
-                    GridItem(.flexible(minimum: 180), spacing: 24)  // Add a second GridItem
-                ]
+                GridItem(.flexible(minimum: 180), spacing: 24),
+                GridItem(.flexible(minimum: 180), spacing: 24)
+            ]
+            
             ScrollView {
-                        LazyVGrid(columns: columns, spacing: 24) {
-                            ForEach(molecules) { molecule in
-                                MoleculeButton(molecule: molecule)
-                                    .padding(8)
-                            }
-                        }
-                        .padding()
+                LazyVGrid(columns: columns, spacing: 24) {
+                    ForEach(molecules) { molecule in
+                        MoleculeButton(molecule: molecule)
+                            .padding(8)
                     }
-                    .edgesIgnoringSafeArea(.bottom)
-                    .padding(.horizontal, 8) // Add horizontal padding
-                    .navigationTitle("Molecules")
+                }
+                .padding()
+            }
+            .edgesIgnoringSafeArea(.bottom)
+            .padding(.horizontal, 8)
+            .navigationTitle("Molecules")
+            .onAppear {
+                loadMolecules()
+            }
         } else {
             ScrollView {
                 LazyVStack(spacing: 24) {
@@ -49,6 +40,20 @@ struct MoleculeView: View {
             }
             .edgesIgnoringSafeArea(.bottom)
             .navigationTitle("Molecules")
+            .onAppear {
+                loadMolecules()
+            }
+        }
+    }
+    
+    func loadMolecules() {
+        guard let url = Bundle.main.url(forResource: "molecules", withExtension: "json") else { return }
+
+        do {
+            let data = try Data(contentsOf: url)
+            molecules = try JSONDecoder().decode([Molecule].self, from: data)
+        } catch {
+            print("Error loading JSON: \(error.localizedDescription)")
         }
     }
 }
