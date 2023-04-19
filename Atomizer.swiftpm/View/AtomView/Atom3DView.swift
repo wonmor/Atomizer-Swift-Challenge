8f89d9b17eb7d001caadb2bd0304a2fdf2d2a447
John Seong <wonmor@gmail.com>
2023-04-18 23:30:23 -0400

    Added camera movement

--- Atomizer.swiftpm/View/AtomView/Atom3DView.swift
+++ Atomizer.swiftpm/View/AtomView/Atom3DView.swift
@@ -42,10 +42,62 @@ struct Atom3DView: UIViewRepresentable {
         ambientLightNode.light?.intensity = 100
         scene.rootNode.addChildNode(ambientLightNode)
         
+        let blackPlaneGeometry = SCNPlane(width: 100, height: 100)
+        let blackPlaneMaterial = SCNMaterial()
+        blackPlaneMaterial.diffuse.contents = UIColor.black
+        blackPlaneGeometry.materials = [blackPlaneMaterial]
+        
         let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
         cameraNode.camera?.fieldOfView = 60
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 3)
         scene.rootNode.addChildNode(cameraNode)
+    
+        // Animate camera zoom in and out
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = 10.0 // Total animation duration
+        let timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        SCNTransaction.animationTimingFunction = timingFunction
+
+        
+        // Zoom in for first 2 seconds
+        SCNTransaction.animationDuration = 2.0
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 2)
+        SCNTransaction.commit()
+        
+        // Oscillate between zoomed in and zoomed out versions
+        let waitDuration = 8.0 // Total duration of each zoom level
+
+        let zoomInPosition = SCNVector3(x: 0, y: 0, z: 1.5)
+        let zoomOutPosition = SCNVector3(x: 0, y: 0, z: 3)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+            let zoomInAction = SCNAction.move(to: zoomInPosition, duration: waitDuration * 0.3)
+            zoomInAction.timingMode = .easeInEaseOut
+
+            let zoomOutAction = SCNAction.move(to: zoomOutPosition, duration: waitDuration * 0.7)
+            zoomOutAction.timingMode = .easeInEaseOut
+
+            let fadeInAction = SCNAction.fadeIn(duration: waitDuration * 0.3)
+            fadeInAction.timingMode = .easeInEaseOut
+
+            let fadeOutAction = SCNAction.fadeOut(duration: waitDuration * 0.7)
+            fadeOutAction.timingMode = .easeInEaseOut
+
+            let sequence = SCNAction.sequence([fadeOutAction, zoomInAction, fadeInAction, zoomOutAction])
+            let repeatAction = SCNAction.repeatForever(sequence)
+
+            cameraNode.runAction(repeatAction, forKey: "zoom")
+        }
+    }
+}
+
+extension SCNVector3 {
+    // Linear interpolation between two vectors
+    func lerp(to: SCNVector3, progress: Float) -> SCNVector3 {
+        return SCNVector3(
+            x: self.x + (to.x - self.x) * progress,
+            y: self.y + (to.y - self.y) * progress,
+            z: self.z + (to.z - self.z) * progress
+        )
     }
 }
