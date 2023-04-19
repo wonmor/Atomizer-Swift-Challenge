a604e2fc2ff12adaad11c812d35ab1ca237991c2
John Seong <wonmor@gmail.com>
2023-04-19 14:08:10 -0400

    Added a ton of comments

--- Atomizer.swiftpm/MyApp.swift
+++ Atomizer.swiftpm/MyApp.swift
@@ -1,20 +1,34 @@
 import SwiftUI
 
 /**
+    ATOMIZER: Visualizing Quantum Mechanics.
     Developed by John Seong, 2023. A WWDC 2023 Swift Student Challenge Submission.
+    Built using Xcode 14 on macOS Ventura, designed to run on iPhone, iPad, and ARM-based Macs.
 
-    Atomizer is a SwiftUI app that visualizes atomic orbitals and molecular orbitals.
+    Atomizer is a state-of-the-art SwiftUI app that visualizes atomic and molecular orbitals.
     It features atomic orbitals in the form of electron density, and molecular orbitals in which you can view it in AR.
-    ARKit and SceneKit are used to render the 3D models. Object occlusion is also supported, despite the fact that only RealityKit supports it.
-    For the data, it uses the ElectronVisualized REST API created from scratch by myself, using Python and Flask.
+ 
+    - ARKit and SceneKit are used to render the 3D models. Object occlusion is also supported on LiDAR-enabled devices, despite the fact that only RealityKit supports it.
+ 
+    - Using Apple's powerful ML-trained Vision framework, I was able to achieve object spawning by hand detection,
+    in which the molecule will come right into your hand like Thor's hammer, all in Augmented Reality.
+ 
+    - For the electron coordinate data, it uses the ElectronVisualized API I created from scratch by myself, using Python and Flask.
+ 
+    Target audience:
+    High school/Undergraduate students, computational chemists, or anyone who's interested in learning more about quantum mechanics.
+ 
+    Disclaimer:
+    DON'T WORRY, INTERNET CONNECTION IS *NOT* REQUIRED TO RUN THIS APP.
+    I PRE-LOADED ALL THE ASSETS FOR THE 3D PLOTTING.
 
-    I used ASE and GPAW to get electron density data, using Density Functional Theory (DFT).
+    - I used ASE and GPAW to get electron density data, using Density Functional Theory (DFT).
     For the molecular orbitals, I used PySCF to get the molecular orbitals, using Hartree–Fock (HF) theory.
 
-    For atomic orbitals, I used the spherical harmonics to compute the radial part of the atomic orbitals.
+    - For atomic orbitals, I used the spherical harmonics to compute the radial part of the atomic orbitals.
     Then, I sampled the wavefunction by using the Metropolis-Hastings algorithm.
 
-    GitHub repo of the API that I created:
+    GitHub repo of the ElectronVisualized API that I created:
     https://github.com/wonmor/ElectronVisualized
 
     Relevant links:
@@ -22,6 +36,11 @@ import SwiftUI
     https://en.wikipedia.org/wiki/Metropolis–Hastings_algorithm
     https://en.wikipedia.org/wiki/Density_functional_theory
     https://en.wikipedia.org/wiki/Hartree%E2%80%93Fock_method
+ 
+    Warm regards,
+ 
+    John Seong
+    Class of 2023 at Garth Webb Secondary School
 */
 
 @main
