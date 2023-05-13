538355c377363474a5d7205707d6bd74f74f583e
John Seong <wonmor@gmail.com>
2023-05-13 13:30:07 -0400

    asdfasdf

--- Atomizer.swiftpm/View/MoleculeView/EnergyViewSheet.swift
+++ Atomizer.swiftpm/View/MoleculeView/EnergyViewSheet.swift
@@ -1,10 +1,6 @@
 import SwiftUI
 import Kingfisher
 
-/**
-    A view that displays an article.
-*/
-
 struct EnergyViewSheet: View {
     let molecule: Molecule
     @Binding var isEnergyView: Bool
@@ -15,43 +11,49 @@ struct EnergyViewSheet: View {
     let localizationManager = LocalizationManager.shared
     
     var body: some View {
-        VStack {
-            KFImage(URL(string: "https://electronvisual.org/api/downloadPNG/ethene_energy_diagram"))
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
+        ZStack {
+            Color(.white)
+                .edgesIgnoringSafeArea(.all)
             
-            Text(localizationManager.localizedString(for: "energy-level-diagram"))
-                .font(.largeTitle)
-                .padding()
-            
-            Button(action: {
-                isEnergyView = false
-            }) {
-                HStack {
-                    Image(systemName: "x.circle.fill")
-                        .font(.system(size: 24))
-                        .foregroundColor(.white)
-                        .padding(16)
-                    Text("Close")
-                        .foregroundColor(.white)
-                        .font(.headline)
-                        .padding(.trailing, 16)
-                        .padding(.vertical, 8)
-                }
-                .background(BlurView(style: .systemMaterialDark).opacity(0.8))
-                .cornerRadius(20)
-                .padding(20)
-                .shadow(radius: 10)
-                .onTapGesture {
+            VStack(alignment: .center) {
+                KFImage(URL(string: "https://electronvisual.org/api/downloadPNG/\(molecule.name.lowercased().trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: " ", with: "_"))_energy_diagram"))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                
+                Text(localizationManager.localizedString(for: "energy-level-diagram"))
+                    .font(.largeTitle)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                
+                Text("This diagram depicts the energy levels of \(molecule.name.lowercased()). Each step in the diagram represents a change in energy state.")
+                    .font(.body)
+                    .padding(.horizontal)
+                    .multilineTextAlignment(.center)
+                
+                Button(action: {
                     withAnimation(Animation.easeInOut(duration: 0.5)) {
                         isEnergyView = false
                     }
+                }) {
+                    HStack {
+                        Image(systemName: "x.circle.fill")
+                            .font(.system(size: 24))
+                            .foregroundColor(.white)
+                        Text("Close")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                    .padding(16)
+                    .background(BlurView(style: .systemMaterialDark).opacity(0.8))
+                    .cornerRadius(20)
+                    .shadow(radius: 10)
                 }
+                .padding()
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+            .foregroundColor(Color.black)
             .padding()
-            
         }
     }
 }
