268da7a6af51e48301a8ba0b2165275b7346fa26
John Seong <wonmor@gmail.com>
2023-07-03 11:40:52 -0400

    Added narration

--- Atomizer.swiftpm/View/MemberView.swift
+++ Atomizer.swiftpm/View/MemberView.swift
@@ -1,6 +1,9 @@
 import SwiftUI
+import AVFoundation
 
 struct MemberView: View {
+    @State private var audioPlayer: AVAudioPlayer?
+    
     var body: some View {
         VStack {
             Spacer()
@@ -16,6 +19,7 @@ struct MemberView: View {
             
             Button(action: {
                 // Implement your payment logic here
+                playAudio()
             }) {
                 Text("Subscribe")
                     .foregroundColor(.white)
@@ -29,9 +33,32 @@ struct MemberView: View {
         }
         .padding()
         .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            playAudio()
+        }
+        .onDisappear {
+            stopAudio()
+        }
+    }
+    
+    func playAudio() {
+        guard let url = Bundle.main.url(forResource: "narration", withExtension: "mp3") else {
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            print("Failed to play audio: \(error.localizedDescription)")
+        }
+    }
+    
+    func stopAudio() {
+        audioPlayer?.stop()
+        audioPlayer = nil
     }
 }
-
 
 struct MemberView_Previews: PreviewProvider {
     static var previews: some View {
