172f5af91c38e9359dc4f9d1d2a9f42271144177
John Seong <wonmor@gmail.com>
2023-07-09 10:19:20 +0900

    Updated StoreManager

--- Atomizer.swiftpm/StoreManager.swift
+++ Atomizer.swiftpm/StoreManager.swift
@@ -51,45 +51,83 @@ class StoreManager: NSObject, ObservableObject, SKPaymentTransactionObserver, SK
     }
     
     func incrementButtonClickCount() {
-           buttonClickCount += 1
-           if buttonClickCount >= 3 {
-               startResetTimer()
-           }
-       }
-       
-   private func startResetTimer() {
-       timeUntilReset = 2 * 60 * 60 // 2 hours in seconds
-       resetTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
-           guard let self = self else { return }
-           if self.timeUntilReset > 0 {
-               self.timeUntilReset -= 1
-           } else {
-               self.resetButtonClickCount()
-           }
-       }
-   }
-   
-   private func resetButtonClickCount() {
-       buttonClickCount = 0
-       resetTimer?.invalidate()
-       resetTimer = nil
-       timeUntilReset = 0
-   }
-
+        buttonClickCount += 1
+        if buttonClickCount >= 3 {
+            startResetTimer()
+        }
+    }
+    
+    private func startResetTimer() {
+        timeUntilReset = 2 * 60 * 60 // 2 hours in seconds
+        resetTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+            guard let self = self else { return }
+            if self.timeUntilReset > 0 {
+                self.timeUntilReset -= 1
+            } else {
+                self.resetButtonClickCount()
+            }
+        }
+    }
+    
+    private func resetButtonClickCount() {
+        buttonClickCount = 0
+        resetTimer?.invalidate()
+        resetTimer = nil
+        timeUntilReset = 0
+    }
+    
     func complete(transaction: SKPaymentTransaction) {
         if let productIdentifier: String? = transaction.payment.productIdentifier {
             purchasedProductIds.insert(productIdentifier!)
+            
+            print("Product Identifier: \(productIdentifier)")
         }
         
-        if let expiryDate = transaction.transactionDate?.addingTimeInterval(1 * 60 * 60 * 24 * 30) { // assuming the subscription is for 1 month
-            subscriptionExpirationDate = expiryDate
+        if let product = products.first(where: { $0.productIdentifier == transaction.payment.productIdentifier }),
+           let duration = product.subscriptionPeriod?.numberOfUnits,
+           let unit = product.subscriptionPeriod?.unit,
+           let component = calendarComponent(for: unit)
+        {
+            let calendar = Calendar.current
+            var components = DateComponents()
+            components.setValue(duration, for: component)
+            
+            if let expiryDate = calendar.date(byAdding: components, to: transaction.transactionDate ?? Date()) {
+                subscriptionExpirationDate = expiryDate
+                
+                print("Expiry Date: \(expiryDate)")
+            }
         }
         
         SKPaymentQueue.default().finishTransaction(transaction)
     }
-
-
+    
+    private func calendarComponent(for unit: SKProduct.PeriodUnit) -> Calendar.Component? {
+        switch unit {
+        case .day:
+            return .day
+        case .week:
+            return .weekOfMonth
+        case .month:
+            return .month
+        case .year:
+            return .year
+        @unknown default:
+            return nil
+        }
+    }
+    
     private func fail(transaction: SKPaymentTransaction) {
         // Handle failed transaction
+        let alert = UIAlertController(title: "Payment Failed",
+                                      message: "Your payment could not be processed.",
+                                      preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okayAction)
+        
+        // Get the top-most view controller to present the alert
+        if let topViewController = UIApplication.shared.windows.first?.rootViewController {
+            topViewController.present(alert, animated: true, completion: nil)
+        }
     }
 }
